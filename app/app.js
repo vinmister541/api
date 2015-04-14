804cf57754f0bfed24bd4c2093fc9600318359d7
vinmister541 <vinmister541@yahoo.com>
2015-04-14 06:17:08 -0400

    initial commit

--- app/app.js
+++ app/app.js
@@ -1,26 +1,11 @@
-$(document).ready(function() {
-
-	$(function(){
-  $('#search-term').submit(function(event){
-    event.preventDefault();
-    var searchTerm = $('#query').val();
-    getRequest(searchTerm);
-  });
+SC.initialize({
+  client_id: '59b223db9c6816b45be8c37fd4cd00bb'
 });
 
-function getRequest(searchTerm){
-  $.getJSON('http://www.khanacademy.org/?s=' + searchTerm + '&r=json', function(data){
-    showResults(data.Search);
+$(document).ready(function() {
+  SC.get('/tracks', { genres: 'zedd' }, function(tracks) {
+  $(tracks).each(function(index, track) {
+  $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+   });
   });
-}
-
-function showResults(results){
-  var html = "";
-  $.each(results, function(index,value){
-    html += '<p>' + value.Title + '</p>';
-    console.log(value.Title);
-  });
-  $('#search-results').html(html);
-}
-	
 });
