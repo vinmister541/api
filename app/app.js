d1cc4aec65642e5519f969b1a630422f9b4132f6
vinmister541 <vinmister541@yahoo.com>
2015-04-16 07:10:25 -0400

    initial commit

--- app/app.js
+++ app/app.js
@@ -2,10 +2,47 @@ SC.initialize({
   client_id: '59b223db9c6816b45be8c37fd4cd00bb'
 });
 
-$(document).ready(function() {
-  SC.get('/tracks', { genres: 'zedd' }, function(tracks) {
+
+
+ $(document).ready(function() {
+
+$('#add').click(function() {
+  var playerInput = $('input#item').val();
+  $("#comment").append(playerInput); 
+
+
+
+  SC.get('https://api.soundcloud.com/tracks', { genres: playerInput }, function(tracks) {
   $(tracks).each(function(index, track) {
-  $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+  var i = track.id;
+  var uniqueTrack = '<button type="button" class="start">' + 'CLICK ME' + '</button>' + '<div id="switch">' + i + '</div>';
+  $('#results').append($('<li></li>').html(track.title + ' - '  + uniqueTrack));
    });
   });
+
+
+
+
+
+
+
+
 });
+
+
+
+
+$('ul').on('click', '.start', function() {
+    $('#main').empty();
+    SC.get('https://api.soundcloud.com/tracks', { genres: playerInput }, function(tracks) {
+   $(tracks).each(function(index, track) {
+   var i = track.id;
+   var manDingo = '<iframe src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + i + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>';
+    $('#main').append($(manDingo));
+  });
+
+ });
+  });
+ });
+
+
